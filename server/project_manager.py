d15506284ce9c506f3af20ef7a5ee0d11d199130
Kayle Gishen <KayleGishen@gmail.com>
2011-03-08 01:18:00 -0500

    Added project file reading, writing, updating

--- server/project_manager.py
+++ server/project_manager.py
@@ -14,11 +14,24 @@ class ProjectManager:
         
         self.projects = os.listdir(self.workspace_path)
         
-    def open_project(self, project_name):
-        self.current_project = project_name
         
     def get_projects(self, format):
         if format == 'json':
             return {
                 'projects': [{ 'name': name } for name in self.projects]
             }
+
+    def read_project_file(self, project):
+        path = os.path.join(self.workspace_path, project, 'project.yaml')
+        return yaml.safe_load(file(path, 'r'))
+        
+    def add_node_to_project(self, project, node):
+        yml = self.read_project_file(project)
+        yml['nodes'][node.keys()[0]] = node[node.keys()[0]]
+        path = os.path.join(self.workspace_path, project, 'project.yaml')
+        handle = open(path, 'w')
+        handle.write(yaml.dump(yml))
+        handle.close()
+        
+    def update_node_in_project(self, project, node):
+        self.add_node_to_porject(project, node)
